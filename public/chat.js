97a49ebfa7f548c63095d84a32eb7b77dd6dc730
Mai Xuan Linh <maixuanlinhdkh@gmail.com>
2023-05-09 00:09:05 +0300

    hide camera, mute, and leave chat features added

--- public/chat.js
+++ public/chat.js
@@ -1,4 +1,4 @@
-let socket = io.connect("http://localhost:4000");
+let socket = io();
 let divVideoChatLobby = document.getElementById("video-chat-lobby");
 let divVideoChat = document.getElementById("video-chat-room");
 let joinButton = document.getElementById("join");
@@ -9,6 +9,14 @@ let roomName;
 let creator = false;
 let rtcPeerConnection;
 let userStream;
+
+let divButtonGroup = document.getElementById("btn-group");
+let muteButton = document.getElementById("muteButton");
+let hideCameraButton = document.getElementById("hideCameraButton");
+let leaveRoomButton = document.getElementById("leaveRoomButton");
+
+let muteFlag = false;
+let hideCameraFlag = false;
 
 // Contains the stun server URL we will be using.
 let iceServers = {
@@ -27,6 +35,52 @@ joinButton.addEventListener("click", function () {
   }
 });
 
+muteButton.addEventListener("click", function () {
+  muteFlag = !muteFlag;
+  if (muteFlag) {
+    userStream.getTracks()[0].enabled = false;
+    muteButton.textContent = "Unmute";
+  } else {
+    userStream.getTracks()[0].enabled = true;
+    muteButton.textContent = "Mute";
+  }
+});
+
+hideCameraButton.addEventListener("click", function () {
+  hideCameraFlag = !hideCameraFlag;
+  if (hideCameraFlag) {
+    userStream.getTracks()[1].enabled = false;
+    hideCameraButton.textContent = "Show Camera";
+  } else {
+    userStream.getTracks()[1].enabled = true;
+    hideCameraButton.textContent = "Hide Camera";
+  }
+});
+
+leaveRoomButton.addEventListener("click", function () {
+  socket.emit("leave", roomName); //Let's the server know that user has left the room.
+
+  divVideoChatLobby.style = "display:block"; //Brings back the Lobby UI
+  divButtonGroup.style = "display:none";
+
+  if (userVideo.srcObject) {
+    userVideo.srcObject.getTracks()[0].stop(); //Stops receiving audio track of User.
+    userVideo.srcObject.getTracks()[1].stop(); //Stops receiving the Video track of User
+  }
+  if (peerVideo.srcObject) {
+    peerVideo.srcObject.getTracks()[0].stop(); //Stops receiving audio track of Peer.
+    peerVideo.srcObject.getTracks()[1].stop(); //Stops receiving the Video track of Peer.
+  }
+
+  //Checks if there is peer on the other side and safely closes the existing connection established with the peer.
+  if (rtcPeerConnection) {
+    rtcPeerConnection.ontrack = null;
+    rtcPeerConnection.onicecandidate = null;
+    rtcPeerConnection.close();
+    rtcPeerConnection = null;
+  }
+});
+
 // Triggered when a room is succesfully created.
 
 socket.on("created", function () {
@@ -35,12 +89,13 @@ socket.on("created", function () {
   navigator.mediaDevices
     .getUserMedia({
       audio: true,
-      video: { width: 1280, height: 720 },
+      video: { width: 500, height: 500 },
     })
     .then(function (stream) {
       /* use the stream */
       userStream = stream;
       divVideoChatLobby.style = "display:none";
+      divButtonGroup.style = "display:flex";
       userVideo.srcObject = stream;
       userVideo.onloadedmetadata = function (e) {
         userVideo.play();
@@ -60,12 +115,13 @@ socket.on("joined", function () {
   navigator.mediaDevices
     .getUserMedia({
       audio: true,
-      video: { width: 1280, height: 720 },
+      video: { width: 500, height: 500 },
     })
     .then(function (stream) {
       /* use the stream */
       userStream = stream;
       divVideoChatLobby.style = "display:none";
+      divButtonGroup.style = "display:flex";
       userVideo.srcObject = stream;
       userVideo.onloadedmetadata = function (e) {
         userVideo.play();
@@ -93,6 +149,8 @@ socket.on("ready", function () {
     rtcPeerConnection.ontrack = OnTrackFunction;
     rtcPeerConnection.addTrack(userStream.getTracks()[0], userStream);
     rtcPeerConnection.addTrack(userStream.getTracks()[1], userStream);
+
+
     rtcPeerConnection
       .createOffer()
       .then((offer) => {
@@ -123,6 +181,7 @@ socket.on("offer", function (offer) {
     rtcPeerConnection.addTrack(userStream.getTracks()[0], userStream);
     rtcPeerConnection.addTrack(userStream.getTracks()[1], userStream);
     rtcPeerConnection.setRemoteDescription(offer);
+
     rtcPeerConnection
       .createAnswer()
       .then((answer) => {
@@ -139,6 +198,25 @@ socket.on("offer", function (offer) {
 
 socket.on("answer", function (answer) {
   rtcPeerConnection.setRemoteDescription(answer);
+});
+
+// Triggered when the other peer in the room has left the room.
+
+socket.on("leave", function () {
+  creator = true; //This person is now the creator because they are the only person in the room.
+  if (peerVideo.srcObject) {
+    peerVideo.srcObject.getTracks()[0].stop(); //Stops receiving audio track of Peer.
+    peerVideo.srcObject.getTracks()[1].stop(); //Stops receiving video track of Peer.
+  }
+
+  //Safely closes the existing connection established with the peer who left.
+
+  if (rtcPeerConnection) {
+    rtcPeerConnection.ontrack = null;
+    rtcPeerConnection.onicecandidate = null;
+    rtcPeerConnection.close();
+    rtcPeerConnection = null;
+  }
 });
 
 // Implementing the OnIceCandidateFunction which is part of the RTCPeerConnection Interface.
